4d34dc22f835ed1b2ccbe9f47e71c9b7dafb4986
shehabkhallaf <shehabkhallaf122@gmail.com>
2022-05-18 22:45:22 +0200

    auto slider

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,29 @@
-let boxes = document.getElementsByClassName("box-item");
+let boxes = document.querySelectorAll(".box-item");
 let indicators = document.getElementsByClassName("slide_btn");
 boxes[0].style.display = "block";
 indicators[0].classList.add("active");
 function slide(sliderIndex) {
-  for (let i = 0; i < boxes.length; i++) {
-    boxes[i].style.display = "none";
-    indicators[i].classList.remove("active");
+  if (sliderIndex) {
+    for (let i = 0; i < boxes.length; i++) {
+      boxes[i].style.display = "none";
+      indicators[i].classList.remove("active");
+    }
+    boxes[sliderIndex].style.display = "block";
+    indicators[sliderIndex].classList.add("active");
+  } else {
+    let i = 0;
+    setInterval(function () {
+      boxes.forEach((box, i) => {
+        box.style.display = "none";
+        indicators[i].classList.remove("active");
+      });
+      if (i == boxes.length) {
+        i = 0;
+      }
+      boxes[i].style.display = "block";
+      indicators[i].classList.add("active");
+      i++;
+    }, 2000);
   }
-  boxes[sliderIndex].style.display = "block";
-  indicators[sliderIndex].classList.add("active");
 }
+slide();
